10854840530af57502299254bcbf9d1a5afcccee
Manohar Kale <manoharkale5368@gmail.com>
2024-12-22 16:53:58 +0530

    22-12-2024

--- db.js
+++ db.js
@@ -7,11 +7,15 @@ main()
   .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect(process.env.MONGODB_URI, {
-    // hosting configuration
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  await mongoose.connect(
+    process.env.MONGODB_URI ||
+      "mongodb+srv://manoharkale5368:YcS2FdrUjxvVnMFr@notebook.golu9.mongodb.net/?retryWrites=true&w=majority&appName=Notebook",
+    {
+      // hosting configuration
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
 }
 
 module.exports = main;
