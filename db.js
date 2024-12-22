fb86ffc509ae20ebbd4bd0f2bf10c9f7bc727f97
Manohar Kale <manoharkale5368@gmail.com>
2024-12-22 16:56:00 +0530

    22-12-2024

--- db.js
+++ db.js
@@ -8,8 +8,7 @@ main()
 
 async function main() {
   await mongoose.connect(
-    process.env.MONGODB_URI ||
-      "mongodb+srv://manoharkale5368:YcS2FdrUjxvVnMFr@notebook.golu9.mongodb.net/?retryWrites=true&w=majority&appName=Notebook",
+    "mongodb+srv://manoharkale5368:YcS2FdrUjxvVnMFr@notebook.golu9.mongodb.net/?retryWrites=true&w=majority&appName=Notebook",
     {
       // hosting configuration
       useNewUrlParser: true,
